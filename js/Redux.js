422e703a5b88a5c7ed048348d06e89864ed98531
Mozhgan <mojganshah@gmail.com>
2025-02-10 11:02:12 +0330

    creating the stores wihout library

---
+++ js/Redux.js
@@ -0,0 +1,20 @@
+const createStore = (reducer) => {
+  let state;
+
+  function dispatch() {
+    // coding
+  }
+
+  function getState() {
+    // coding
+  }
+
+  return {
+    dispatch,
+    getState,
+  };
+};
+
+let store = createStore(() => {});
+
+console.log(store);
